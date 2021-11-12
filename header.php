a438a8840e2c431ce8a21d615a6e848f4f3129ac
sonledinh <sonledinh@admicro.vn>
2021-11-12 20:38:55 +0700

    update final home

--- header.php
+++ header.php
@@ -16,4 +16,67 @@
     <script type="text/javascript" src="js/lib/jquery.min.js"></script> 
 </head>
 <body>  
-    <header></header>
+    <header>
+        <div class="header-pc">
+            <div class="container">
+                <div class="header-top">
+                    <div class="logo"><a href=""><img src="images/logo.png" class="img-fluid" alt=""></a></div>
+                    <div class="head-right">
+                        <div class="h-top">
+                            <div class="left">Bình Dung hân hạnh kính chào quý khách !</div>
+                            <div class="right">
+                                <div class="hotline">
+                                    <span>HOTLINE:</span>
+                                    <div class="phone-line">
+                                        <p>0937 655 039 ( Mr. Long )</p>
+                                        <p>0937 535 039 ( Mrs. Thùy Dung )</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="h-menu">
+                            <ul>
+                                <li class="active"><a href="">Trang chủ</a></li>
+                                <li><a href="">Giới thiệu</a></li>
+                                <li><a href="">Sản phẩm</a></li>
+                                <li><a href="">Dịch vụ</a></li>
+                                <li><a href="">Tin tức</a></li>
+                                <li><a href="">Liên hệ</a></li>
+                            </ul>
+                            <div class="box-search">
+                                <input type="text" placeholder="Tìm kiếm ...">
+                                <button><i class="fa fa-search"></i></button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="header-mobile" style="display: none;">
+            <div class="container">
+                <div class="row align-items-center">
+                    <div class="col-6 col-sm-6 col-md-6">
+                        <div class="logo"><a href=""><img src="images/logo.png" class="img-fluid" alt=""></a></div>
+                    </div>
+                    <div class="col-6 col-sm-6 col-md-6">
+                        <div class="btn-menu text-right"><a href="#menu"><i class="fa fa-bars"></i></a></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <nav id="menu">
+            <ul>
+                <li>
+                    <input type="text" placeholder="Tìm kiếm">
+                    <button><i class="fa fa-search"></i></button>
+                </li>
+                <li><a href="">Trang chủ</a></li>
+                <li><a href="">Giới thiệu</a></li>
+                <li><a href="">Sản phẩm</a></li>
+                <li><a href="">Dịch vụ</a></li>
+                <li><a href="">Tin tức</a></li>
+                <li><a href="">Liên hệ</a></li>
+            </ul>
+        </nav>
+    </header>
+ 
